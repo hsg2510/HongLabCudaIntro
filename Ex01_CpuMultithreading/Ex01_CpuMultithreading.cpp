bf5e3bf67dfb77480b9006a2c092fc22dfd54bae
DESKTOP-0C1HVVP\hsg2510 <hsg2510@naver.com>
2025-01-01 16:19:47 +0900

    ex01_solution

--- Ex01_CpuMultithreading/Ex01_CpuMultithreading.cpp
+++ Ex01_CpuMultithreading/Ex01_CpuMultithreading.cpp
@@ -21,7 +21,7 @@ int main()
 {
 	using clock = std::chrono::high_resolution_clock;
 
-	//int size = 37;
+	//int size = 39;
 	int size = 1024 * 1024 * 512;
 
 	vector<int> a(size);
@@ -40,26 +40,26 @@ int main()
 	{
 		auto addFunc = [&](int start, int end, int size)
 			{
-				//for (int i = start; i < end; i++)
-				//	if (i < size)
-				//		cMulti[i] = TODO;
+				for (int i = start; i < end; i++)
+					if (i < size)
+						cMulti[i] = a[i] + b[i];
 			};
 
 		printf("Start multithreading\n");
 		auto start = clock::now(); // 시간 측정 시작
 
-		int numThreads = thread::hardware_concurrency();
+		int numThreads = thread::hardware_concurrency(); //CPU 코어개수 얻어오는 함수 정확히는 logical process(하나의 코어 안에 여러개의 logical process가 있음)
 		int perThread = int(ceil(float(size) / numThreads));
 
 		vector<thread> threadList(numThreads);
 
 		for (int r = 0; r < 100; r++) // 한 번으로는 CPU 사용량 관찰이 쉽지 않기 때문에 여러 번 반복
 		{
-			//for (int t = 0; t < numThreads; t++)
-			//    threadList[t] = thread(addFunc, TODO, TODO, size);
+			for (int t = 0; t < numThreads; t++)
+			    threadList[t] = thread(addFunc, t * perThread, (t+1) * perThread, size);
 
-			//for (int t = 0; t < numThreads; t++)
-			//    threadList[t].join();
+			for (int t = 0; t < numThreads; t++)
+			    threadList[t].join();
 		}
 
 		printf("Time taken: %f ms\n", duration<float, milli>(clock::now() - start).count());
